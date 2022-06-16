e65ee2b097fcac17be925546d85b7c79c66314e7
Abhijeet <dsisavi@gmail.com>
2022-06-16 22:49:50 +0200

    Added user validation logic

--- functions/src/entities.ts
+++ functions/src/entities.ts
@@ -1,10 +1,10 @@
-export const verifyToken = "VV";
+export const verifyToken = "ZZZ";
 export const requestLogCollection = "/request-log";
 export const requestMessageCollection =
-   (documentId:string) =>
-     `${requestLogCollection}/${documentId}/unparsed-message`;
+  (documentId: string) =>
+    `${requestLogCollection}/${documentId}/unparsed-message`;
 
-export const messageRecord = (messageText:string):MessageRecord => {
+export const messageRecord = (messageText: string): MessageRecord => {
   return {message: messageText};
 };
 
@@ -12,54 +12,90 @@ interface MessageRecord {
   message: string,
 }
 
-export interface FunctionResponse{
-    message: string,
-    httpStatus: number;
+export interface FunctionResponse {
+  message: string,
+  httpStatus: number;
 }
 
-export interface PostRequest{
+export interface PostRequest {
   entry: Array<ChangeRequestObject>,
   object: string
 
 }
 
-export interface ChangeRequestObject{
+export interface ChangeRequestObject {
   changes: ChangeRequest[]
 }
 
-export interface ChangeRequest{
+export interface ChangeRequest {
 
-        field: string,
-        value: MessageValue;
+  field: string,
+  value: MessageValue;
 
 }
 
-export interface MessageValue{
+export interface MessageValue {
 
-    messaging_product: string,
-    metadata: MessageMetada,
-    contacts:Contact[],
-    messages: MessageObject[];
+  messaging_product: string,
+  metadata: MessageMetada,
+  contacts: Contact[],
+  messages: MessageObject[];
 }
 
-export interface MessageMetada{
-    display_phone_number:string,
-    phone_number_id: string;
-  }
+export interface MessageMetada {
+  display_phone_number: string,
+  phone_number_id: string;
+}
 
-export interface MessageObject{
-    from: string,
-    id: string,
-    timestamp: string,
-    type: string,
-    text: Text;
-  }
-export interface Text{
-      body: string;
-  }
-export interface Contact{
-    profile: {
-          name :string
-        },
-        wa_id: string;
+export interface MessageObject {
+  from: string,
+  id: string,
+  timestamp: string,
+  type: string,
+  text: Text;
+}
+export interface Text {
+  body: string;
+}
+export interface Contact {
+  profile: {
+    name: string
+  },
+  wa_id: string;
+}
+
+export interface user {
+  name: string,
+  phone: string
+}
+export const usersPresent = [
+  {
+    phoneNumber: "ZZZ",
+    name: "Abhijeet",
+  },
+  {
+    name: "Sravya",
+    phoneNumber: "ZZZ",
+  },
+];
+
+export enum splitStratergy {
+  Both,
+  Single
+}
+
+export interface tempSpendObject {
+  amount: number
+  split: splitStratergy,
+  description: string,
+}
+
+
+export interface theSpend {
+  amount: number
+  spender: user
+  split: splitStratergy,
+  description: string
+  date: Date
+  messageRef: string
 }
--- functions/src/posthandler.ts
+++ functions/src/posthandler.ts
@@ -1,20 +1,32 @@
-import {FunctionResponse, PostRequest, requestLogCollection,
-  requestMessageCollection, messageRecord}
+import {
+  FunctionResponse, PostRequest, requestLogCollection,
+  requestMessageCollection, messageRecord, splitStratergy,
+  user, tempSpendObject, theSpend, usersPresent,
+}
   from "./entities";
 import {Request} from "firebase-functions/v1";
 import * as functions from "firebase-functions";
 import * as db from "./databaseAccess";
 
+const getDateFromEpoch = (time: number) => {
+  const date: Date = new Date(0);
+  date.setUTCSeconds(time);
+  return date;
+};
+
 /**
  *
- * @param {Request} postRequest: This is the request we will get from Webhook
+ * @param {Request} postRequest: This is the
+ * request we will get from Webhook
  * @return {FunctionResponse}: Response to this request
  */
 export async function
-postHandler(postRequest:Request):Promise<FunctionResponse> {
+postHandler(postRequest: Request): Promise<FunctionResponse> {
   const FbWebhookMessage: PostRequest = postRequest.body as PostRequest;
-  functions.logger.info(postRequest.body??"asd");
-  const requestRef:Promise<string> = db.writeToCollection(
+  const user: user = validateAndReturnUser(FbWebhookMessage);
+
+  // functions.logger.info(postRequest.body ?? "asd");
+  const requestRef: Promise<string> = db.writeToCollection(
       requestLogCollection, FbWebhookMessage);
 
   let message: string;
@@ -32,12 +44,94 @@ postHandler(postRequest:Request):Promise<FunctionResponse> {
   }
   functions.logger.info(`Message is == ${message}`);
   const requestId = await requestRef;
-  const messageRef:string = await db
+  const messageRef: string = await db
       .writeToCollection(
           requestMessageCollection(requestId), messageRecord(message));
-  functions.logger.error(`${messageRef} + ${requestId}`);
+
+
+  const messageTime: number = +FbWebhookMessage
+      .entry[0]
+      .changes[0]
+      .value
+      .messages[0]
+      .timestamp;
+  const parsedMessage = parseMessage(message);
+  functions.logger.debug("parsed message ", parsedMessage);
+  const finalMessage: theSpend = {
+    amount: parsedMessage.amount,
+    spender: user,
+    split: parsedMessage.split,
+    description: parsedMessage.description,
+    date: getDateFromEpoch(messageTime),
+    messageRef: messageRef,
+  };
+
+  db.writeToCollection("/the-spend", finalMessage);
   return {
-    message: `${messageRef}`,
+    message: "Mission Successfull",
     httpStatus: 201,
+  };
+}
+
+
+/**
+ *
+ * @param {string} message: Message as recieved from webhook
+ * @return {theSpend}: The spend object
+ */
+function parseMessage(message: string): tempSpendObject {
+  const sanatizedMessage = message.trim();
+  let split: splitStratergy = splitStratergy.Single;
+  const splitMessage = sanatizedMessage.split(" ");
+  let description = "";
+  let amount: number = Number.MAX_SAFE_INTEGER;
+  functions.logger.debug("Parsing tokens");
+  for (const token of splitMessage) {
+    functions.logger.debug(token);
+    if (token.toUpperCase() == "BOTH") {
+      split = splitStratergy.Both;
+    } else if (+token) {
+      amount = +token;
+    } else {
+      description = `${description} ${token}`;
+    }
+  }
+  if (amount == Number.MAX_SAFE_INTEGER) {
+    throw new Error("Could not extract amount");
+  }
+
+  return {
+    amount: amount,
+    split: split,
+    description: description,
+  };
+}
+
+/**
+ *
+ * @param {PostRequest} fbRequest: The message request we got
+ * @return {users} the user the message was for
+ * It should throw an exception if the user is not one of me or sravya
+ */
+function validateAndReturnUser(fbRequest: PostRequest): user {
+  const number = fbRequest
+      .entry[0]
+      .changes[0]
+      .value
+      .messages[0]
+      .from;
+
+  const user = usersPresent.find((us) => us.phoneNumber == number);
+
+  if (!user) {
+    const errorMessage = `Not a valid user.
+     Couldnt find a user with phone number ${number}`;
+    functions.logger.error(errorMessage);
+    throw new Error(errorMessage);
+  }
+
+  return {
+    name: user.name,
+    phone: user.phoneNumber,
   };
 }
